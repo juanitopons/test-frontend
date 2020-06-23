3a418397ea03f4779a01bbf4eb3f143a173f1e74
Juanito <juanpons1459@gmail.com>
2020-06-23 15:11:45 +0200

    Add code lint fixes changes for current linter rules

--- api/server.js
+++ api/server.js
@@ -1,18 +1,18 @@
-const path = require('path')
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'events.json'))
-const middlewares = jsonServer.defaults()
-const PORT = 4000
+const path = require('path');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'events.json'));
+const middlewares = jsonServer.defaults();
+const PORT = 4000;
 
 server.use((req, res, next) => {
-  delete req.query.start
-  delete req.query.end
-  next()
-})
+  delete req.query.start;
+  delete req.query.end;
+  next();
+});
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
-})
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
